3f58c1f683fcbb4db73a0c7377eda72a0a73eec7
Takuto Amagasa <takuto.amagasa@samurai-soft.com>
2025-07-06 18:37:48 +0900

    first

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {}
+
+module.export = nextConfig;
+
+async () => {
+    try {
+        const { initOpenNextCloudflareForDev} = await import("@opennextjs/cloudflare");
+        initOpenNextCloudflareForDev();
+    } catch (error) {
+        console.error(error);
+    }
+};
